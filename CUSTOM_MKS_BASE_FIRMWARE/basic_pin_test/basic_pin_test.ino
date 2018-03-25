2553afb859c9b8dda2420d5c8ae90791299fe240
Brad Dranko <bddranko@gmail.com>
2018-03-25 19:11:04 -0400

    Added cam light function to Control board firmware

--- CUSTOM_MKS_BASE_FIRMWARE/basic_pin_test/basic_pin_test.ino
+++ CUSTOM_MKS_BASE_FIRMWARE/basic_pin_test/basic_pin_test.ino
@@ -1,3 +1,6 @@
+#include <Adafruit_NeoPixel.h>
+
+
 #define X_STEP_PIN         54
 #define X_DIR_PIN          55
 #define X_ENABLE_PIN       38
@@ -41,33 +44,56 @@
 
 #define BEEPER_PIN 37
 #define HEATER_BED_PIN    8    // BED
-void setup() {
-  Serial.begin(9600);
-  Serial.println("Testing....");
+//****************************************** END OF MKS BASE MARLIN PIN DEFINITIONS ************************************
 
-// x axis pin modes
-pinMode(X_STEP_PIN,OUTPUT);      
+//WS2812 LED ring for camera lighting
+#define CAM_LED A11
+#define CAM_LED_NUM 24
+
+
+
+
+
+Adafruit_NeoPixel CamLight = Adafruit_NeoPixel(CAM_LED_NUM, CAM_LED, NEO_GRB + NEO_KHZ800);
+
+
+void setup() {
+Serial.begin(115200);
+CamLight.begin();
+Serial.println("Running.........");
+setAllColor(); // turns on Cam Light Ring
+
+
+// X axis stepper driver pins
+pinMode(X_STEP_PIN,OUTPUT);  
 pinMode(X_DIR_PIN,OUTPUT);      
 pinMode(X_ENABLE_PIN,OUTPUT);   
 pinMode(X_MIN_PIN,INPUT);        
 pinMode(X_MAX_PIN,INPUT);
 
+// Y axis stepper driver pins
 pinMode(Y_STEP_PIN,OUTPUT);      
 pinMode(Y_DIR_PIN,OUTPUT);      
 pinMode(Y_ENABLE_PIN,OUTPUT);      
 pinMode(Y_MIN_PIN,INPUT);         
 pinMode(Y_MAX_PIN,INPUT);          
 
+
+//Z axis stepper driver pins
 pinMode(Z_STEP_PIN,OUTPUT);         
 pinMode(Z_DIR_PIN,OUTPUT);          
 pinMode(Z_ENABLE_PIN,OUTPUT);      
 pinMode(Z_MIN_PIN,INPUT);          
 pinMode(Z_MAX_PIN,INPUT);          
 
+
+//E0 axis stepper driver pins
 pinMode(E0_STEP_PIN,OUTPUT);        
 pinMode(E0_DIR_PIN,OUTPUT);         
 pinMode(E0_ENABLE_PIN,OUTPUT);      
 
+
+//E1 axis stepper driver pins
 pinMode(E1_STEP_PIN,OUTPUT);       
 pinMode(E1_DIR_PIN,OUTPUT);        
 pinMode(E1_ENABLE_PIN,OUTPUT);      
@@ -93,7 +119,7 @@ delay(1000);
 }
 
 
-void pulse(int stepperSelect1,int stepperSelect2,int steppselect3, int dist){
+void pulse(int stepperSelect1, int dist){
   int count = 0;
 
   while(count < dist){
