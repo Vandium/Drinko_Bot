8f693090c85defa20fb5b0f09d2a40c5b39f7eb7
Brad Dranko <bddranko@gmail.com>
2018-03-27 21:54:12 -0400

    Fixed errors

--- CUSTOM_MKS_BASE_FIRMWARE/Drinko_Firmware/WS2812_Functions.ino
+++ CUSTOM_MKS_BASE_FIRMWARE/Drinko_Firmware/WS2812_Functions.ino
@@ -6,9 +6,8 @@ for(int i=0;i<CAM_LED_NUM;i++){
 
     // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
     CamLight.setPixelColor(i, CamLight.Color(RED,GREEN,BLUE)); // Moderately bright green color.
-
-    CamLight.show(); // This sends the updated pixel color to the hardware.
-
-
   }
-}
+
+  CamLight.show(); // This sends the updated pixel color to the hardware.
+
+}// setAllColor END
